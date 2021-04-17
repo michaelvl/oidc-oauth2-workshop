991eb94339430ceee1dbe73f8d50c94015a021ca
MichaelVL <mvl.gh@network42.dk>
2021-04-17 09:11:39 +0200

    Switch to use external environment variables

---
+++ configs/local-idp.sh
@@ -0,0 +1,7 @@
+export OAUTH2_URL=http://localhost:5000/authorize
+export OAUTH2_TOKEN_URL=http://localhost:5000/token
+export OIDC_JWKS_URL=http://localhost:5000/.well-known/jwks.json
+export CLIENT_ID=client-123-id
+export CLIENT_SECRET=client-123-password
+
+export API_BASE_URL=http://localhost:5002
