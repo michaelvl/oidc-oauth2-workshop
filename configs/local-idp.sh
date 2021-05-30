f0d49f91875823bb4bffa99320b23da350fa17c4
MichaelVL <mvl.gh@network42.dk>
2021-05-30 11:29:42 +0200

    Local IDP to use autoconfig

--- configs/local-idp.sh
+++ configs/local-idp.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-export OAUTH2_URL=http://localhost:5001/authorize
-export OAUTH2_TOKEN_URL=http://localhost:5001/token
-export OAUTH2_USERINFO_URL=http://localhost:5001/userinfo
-export OIDC_JWKS_URL=http://localhost:5001/.well-known/jwks.json
+source configs/oidc-autoconfig.sh localhost:5001
 export CLIENT_ID=client-123-id
 export CLIENT_SECRET=client-123-password
 
